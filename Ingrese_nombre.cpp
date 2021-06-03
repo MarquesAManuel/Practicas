16a5172f70933eb73462933c1264ce654736fcc2
ManuelMarques <MANU_J55@live.com.ar>
2021-06-03 19:12:03 -0300

    unidad_funciones

---
+++ Ingrese_nombre.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std
+
+
+int main()
+{
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello " << name << endl;
+
+    cin.ignore();
+    cin.get();	
+
+    return 0;
+}
