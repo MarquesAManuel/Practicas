83d603fc80d9d356e6d33cb035587510c19f3b78
Manuel Marques <MANU_J55@live.com.ar>
2021-06-05 16:11:06 -0300

    volumen_conito

--- volumen_cono.cpp
+++ volumen_cono.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 
 int main()
 {
-   double radio, altura;
-   radio = 14.5;
-   altura = 26.79;
-   cout << " El volumen del cono es = "<< (3.14*(radio*2)/altura)/3 << endl;
+
+   double radio = 14.5;
+   double alCuadrado = pow(radio,2);
+   double altura = 26.79;
+   double pi = 3.14;
+   cout << " El volumen del cono es = "<< (pi*(alCuadrado)*altura)/3 << endl;
 
     return 0;
 }
