2a999753b30bcb5e2d2063121cf0ce8b0ef83315
Manuel Marques <MANU_J55@live.com.ar>
2021-06-05 13:36:51 -0300

    volumen

---
+++ volumen_cono.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   double radio, altura;
+   radio = 14.5;
+   altura = 26.79;
+   cout << " El volumen del cono es = "<< (3.14*(radio*2)/altura)/3 << endl;
+
+    return 0;
+}
